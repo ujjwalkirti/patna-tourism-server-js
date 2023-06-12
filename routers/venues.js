9d09537f16d8abfa9cb70c0aa165df286873b624
Ujjwal Kirti <64329707+ujjwalkirti@users.noreply.github.com>
2023-06-12 11:54:21 +0530

    Chore: remove passport.js based authentication and added some more endpoints

--- routers/venues.js
+++ routers/venues.js
@@ -1,71 +1,127 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const { Venue } = require("../utils/schema");
+const { Venue, Review } = require("../utils/schema");
 const express = require("express");
 const router = express.Router();
 const { validateApiKey } = require("../utils/middlewares");
-router.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+router.get("/", (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const venues = yield Venue.find({});
-        return res.status(200).json(venues);
+      const venues = yield Venue.find({});
+      return res.status(200).json(venues);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error retrieving venues",
+        error,
+      });
     }
-    catch (error) {
-        return res.status(500).json({
-            message: "Error retrieving venues",
-            error,
-        });
-    }
-}));
-router.post("/", validateApiKey, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    const venues = await Venue.findById(id);
+    return res.status(200).json(venues);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error retrieving venue with id:" + req.params.id,
+      error,
+    });
+  }
+});
+
+router.get("/:id/reviews", async (req, res) => {
+  try {
+    const id = new mongoose_1.Types.ObjectId(req.params.id);
+    const reviews = await Review.find({ id });
+    return res.status(200).json(reviews);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error retrieving reviews for venue with id:" + req.params.id,
+      error,
+    });
+  }
+});
+
+router.post("/", validateApiKey, (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const newVenue = new Venue(req.body);
-        const savedVenue = yield newVenue.save();
-        return res.status(201).json(savedVenue);
+      const newVenue = new Venue(req.body);
+      const savedVenue = yield newVenue.save();
+      return res.status(201).json(savedVenue);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Sorry, something went wrong.", error });
     }
-    catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Sorry, something went wrong.", error });
-    }
-}));
-router.patch("/:id", validateApiKey, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+router.patch("/:id", validateApiKey, (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const id = new mongoose_1.default.Types.ObjectId(req.params.id);
-        const updatedVenue = yield Venue.findByIdAndUpdate(id, req.body, {
-            runValidators: true,
-        });
-        return res.status(200).json(updatedVenue);
+      const id = new mongoose_1.default.Types.ObjectId(req.params.id);
+      const updatedVenue = yield Venue.findByIdAndUpdate(id, req.body, {
+        runValidators: true,
+      });
+      return res.status(200).json(updatedVenue);
+    } catch (error) {
+      return res.status(500).json(error);
     }
-    catch (error) {
-        return res.status(500).json(error);
-    }
-}));
-router.delete("/:id", validateApiKey, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+router.delete("/:id", validateApiKey, (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const venueId = new mongoose_1.default.Types.ObjectId(req.params.id);
-        const deletedVenue = yield Venue.findByIdAndDelete(venueId);
-        if (deletedVenue) {
-            res.json({ message: "Venue deleted successfully" });
-        }
-        else {
-            res.status(404).json({ message: "Venue not found" });
-        }
+      const venueId = new mongoose_1.default.Types.ObjectId(req.params.id);
+      const deletedVenue = yield Venue.findByIdAndDelete(venueId);
+      if (deletedVenue) {
+        res.json({ message: "Venue deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Venue not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error deleting venue", error });
     }
-    catch (error) {
-        res.status(500).json({ message: "Error deleting venue", error });
-    }
-}));
+  })
+);
 module.exports = router;
--- utils/schema.js
+++ utils/schema.js
@@ -1,90 +1,114 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 const mongoose_1 = require("mongoose");
 // Define the user schema
 const typeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
 });
 const venueSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        default: 0,
-        max: 10,
-    },
-    lat: {
-        type: Number,
-        required: true,
-    },
-    long: {
-        type: Number,
-        required: true,
-    },
-    img: {
-        type: String,
-        required: true,
-    },
-    website: {
-        type: String,
-        default: "",
-    },
+  type: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    max: 10,
+  },
+  lat: {
+    type: Number,
+    required: true,
+  },
+  long: {
+    type: Number,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+    default: "",
+  },
 });
 const reviewSchema = new mongoose.Schema({
-    venue: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Venue",
-        required: true,
-    },
-    author: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
+  venue: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Venue",
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
 });
 const userSchema = new mongoose_1.Schema({
-    googleId: { type: String, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
+  googleId: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
 });
 // Assuming your User model extends the mongoose.Model class
 userSchema.methods.serializeUser = function () {
-    return this._id; // Serialize the user object by returning the user ID
+  return this._id; // Serialize the user object by returning the user ID
 };
 userSchema.statics.deserializeUser = function (id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return this.findById(id); // Deserialize the user object by querying the database using the user ID
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    return this.findById(id); // Deserialize the user object by querying the database using the user ID
+  });
 };
 const apiKeySchema = new mongoose_1.Schema({
-    key: { type: String, required: true },
-    userId: { type: String, required: true },
+  key: { type: String, required: true },
+  userId: { type: String, required: true },
 });
 // Create the User model based on the user schema
 const Type = mongoose.model("Type", typeSchema);
